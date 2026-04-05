1c811ceef27b3a0d76fd80141eea45194ba8d5ac
alokatulkar <alokatulkar@gmail.com>
2026-04-05 22:12:29 +0530

    modified terraform

--- terraform/eks/main.tf
+++ terraform/eks/main.tf
@@ -76,7 +76,7 @@ resource "aws_eks_node_group" "nodes" {
     min_size     = 1
   }
 
-  instance_types = ["t3.medium"]
+  instance_types = ["c7i-flex.large"]
 
   depends_on = [
     aws_iam_role_policy_attachment.node_policy,
